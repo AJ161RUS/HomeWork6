1efbd2ca8a6469c00f5da5a4d0f9d935b1145b92
Andrey Zhulay <zhulay.00@mail.ru>
2023-03-21 22:19:37 +0300

    second  commit

---
+++ Task_25/25.cs
@@ -0,0 +1,22 @@
+﻿/* Задача 25: Напишите цикл, который принимает на вход два 
+числа (A и B) и возводит число A в натуральную степень B.
+
+3, 5 -> 243 (3⁵)
+
+2, 4 -> 16
+
+*/
+
+int A, B;
+int sum = 1;
+Console.Write("Введите число A: ");
+int.TryParse(Console.ReadLine()!, out A);
+Console.Write("Введите число B: ");
+int.TryParse(Console.ReadLine()!, out B);
+
+for (int i = 0; i < B; i++)
+{
+    sum = A * sum;
+    
+}
+Console.WriteLine($"{sum}");
---
+++ Task_27/27.cs
@@ -0,0 +1,27 @@
+﻿/*Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+452 -> 11
+
+82 -> 10
+
+9012 -> 12
+*/
+/*
+
+
+int Number;
+int n = 0;
+Console.Write("Введите число: ");
+int.TryParse(Console.ReadLine(), out Number);
+Sum(Number, n);
+
+int Sum(int figure, int result)
+{
+    while (figure > 0)
+ {
+     result = result + figure % 10;
+     figure = figure / 10;
+ }
+ return n = result;
+}
+Console.WriteLine($"{Number} -> {n}");
